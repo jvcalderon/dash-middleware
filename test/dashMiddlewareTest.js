b7b134ac3fb123c38b3e800305844ca78176c863
José Vte. Calderón <jvcprod@hotmail.com>
2017-11-30 18:41:51 +0100

    Schema changes

--- test/dashMiddlewareTest.js
+++ test/dashMiddlewareTest.js
@@ -1,6 +1,7 @@
 "use strict"
 
 const assert = require("assert")
+const co = require("co")
 const fs = require("file-system")
 const TMP_DIR = __dirname + '/TMP'
 const ORIGINAL_MOCK_FILE = __dirname + '/dashFileMock.dash'
@@ -30,6 +31,76 @@ describe('DashMiddleware', () => {
                 assert.equal(tags.length, 2)
                 assert.equal(tags[0].get('tag'), 'SQL')
                 assert.equal(tags[1].get('tag'), 'DB')
+            })
+        })
+    })
+
+    describe('#sync()', () => {
+        it('Should create updateAt and createdAt fields', () => {
+            const dash = new DashMiddleware(TMP_DIR + '/' + TEST_MOCK_FILE, {timestamps: true})
+            return co(function *() {
+                yield dash.sync()
+                return dash.Snippet.findAll({include: [dash.Tag]}).then((result) => {
+                    assert.equal(result[0].createdAt, null)
+                    assert.equal(result[0].updatedAt, null)
+                })
+            })
+        })
+        it('Should remove updateAt and createdAt fields', () => {
+            let dash = new DashMiddleware(TMP_DIR + '/' + TEST_MOCK_FILE, {timestamps: true})
+            return co(function *() {
+                yield dash.sync()
+                yield dash.Snippet.findAll({include: [dash.Tag]}).then((result) => {
+                    assert.equal(result[0].createdAt, null)
+                    assert.equal(result[0].updatedAt, null)
+                })
+                dash = new DashMiddleware(TMP_DIR + '/' + TEST_MOCK_FILE, {timestamps: false})
+                yield dash.sync()
+                return dash.Snippet.findAll({include: [dash.Tag]}).then((result) => {
+                    assert.equal(result[0].createdAt, undefined)
+                    assert.equal(result[0].updatedAt, undefined)
+                })
+            })
+        })
+        it('Should update updatedAt field on update', () => {
+            let dash = new DashMiddleware(TMP_DIR + '/' + TEST_MOCK_FILE, {timestamps: true})
+            return co(function *() {
+                yield dash.sync()
+                const snippet = yield dash.Snippet.findOne({title: 'selectsql-'})
+                return yield snippet.updateAttributes({
+                    title: 'mock-',
+                    body: 'Mock body',
+                    syntax: 'text/plain',
+                    tags: [],
+                    usageCount: 0
+                }).then((result) => {
+                    assert.equal(result.createdAt, null)
+                    assert.notEqual(result.updatedAt, null)
+                })
+            })
+        })
+        it('Should set createdAt and updatedAt field on create', () => {
+            let dash = new DashMiddleware(TMP_DIR + '/' + TEST_MOCK_FILE, {timestamps: true})
+            return co(function *() {
+                yield dash.sync({keyFixing: true})
+                dash = new DashMiddleware(TMP_DIR + '/' + TEST_MOCK_FILE, {timestamps: true})
+                return yield dash.Snippet.create({
+                    title: 'newMock-',
+                    body: 'Mock body new',
+                    syntax: 'text/plain',
+                    usageCount: 0
+                }).then((result) => {
+                    assert.notEqual(result.createdAt, null)
+                    assert.notEqual(result.updatedAt, null)
+                })
+            })
+        })
+        it('Should fix/unfix wrong foreign keys', () => {
+            let dash = new DashMiddleware(TMP_DIR + '/' + TEST_MOCK_FILE)
+            return co(function *() {
+                yield dash.sync({keyFixing: true})
+                yield dash.sync({keyFixing: false})
+                console.log('asdsad')
             })
         })
     })
